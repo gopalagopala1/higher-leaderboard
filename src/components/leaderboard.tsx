fa099b441f467061e3bdf2dc8cec0747a64927a0
gopalaxdev <agrawalgopala@gmail.com>
2024-06-20 05:45:20 +0530

    chore: make design responsive

--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -178,109 +178,115 @@ export default function Leaderboard() {
 
   return (
     <AuthKitProvider config={config}>
-      <div className="flex flex-col items-center justify-center max-h-screen text-white p-4">
-        <div className="w-full">
-          <div className="flex space-x-4 mb-8 justify-between items-center">
-            <div className="flex items-center space-x-4">
-              <Image
-                src="/Vector.png"
-                alt="logo"
-                width={50}
-                height={40}
-                style={{ height: "50px" }}
-              />
-              <h1 className="text-4xl font-bold text-[#FEFAE0]">HIGHERBOARD</h1>
-            </div>
+      <div className=" text-white max-h-screen p-4 md:text-xs lg:text-base">
+        <div className="flex space-x-4 mb-8 justify-between items-center">
+          <div className="flex items-center space-x-4">
+            <Image
+              src="/Vector.png"
+              alt="logo"
+              width={50}
+              height={40}
+              style={{ height: "50px" }}
+            />
+            <h1 className="text-4xl font-bold text-[#FEFAE0]">HIGHERBOARD</h1>
+          </div>
 
-            <div className="sign-in-button">
-              <SignInButton onSuccess={onSignInSuccess} onSignOut={onSignOut} />
-            </div>
+          <div className="sign-in-button">
+            <SignInButton onSuccess={onSignInSuccess} onSignOut={onSignOut} />
           </div>
-          <div className="overflow-x-auto bg-black  shadow-md min-h-[calc(100vh-380px)] max-h-[calc(100vh-380px)] min-w-[calc(100vw-450px)] m-auto">
-            <table
-              className={`min-w-full ${kreadonDemi.className} font-semibold text-sm`}
-            >
-              <thead className="text-black">
-                <tr className="bg-green-600">
-                  <th className="py-8 px-4 border-b border-gray-900 border-r">
-                    Rank
-                  </th>
-                  <th className="py-8 px-4 border-b border-gray-900  border-r">
-                    User
-                  </th>
-                  <th className="py-8 px-4 border-b border-gray-900  border-r">
-                    Casts
-                  </th>
-                  <th className="py-8 px-4 border-b border-gray-900  border-r">
-                    Recasts
-                  </th>
-                  <th className="py-8 px-4 border-b border-gray-900  border-r">
-                    Likes
-                  </th>
-                  <th className="py-8 px-4 border-b border-gray-900  border-r">
-                    Replies
-                  </th>
-                </tr>
-              </thead>
-              <tbody className={kreadonDemi.className}>
-                {displayData.map((entry, index) => (
-                  <tr key={index} className="">
-                    <td className="py-8 px-10 border-r border-gray-900  text-center">
-                      {entry.rank}
-                    </td>
-                    <td className="py-8 px-12 border-r border-gray-900  text-center">
-                      <div className="flex items-center space-x-2">
-                        <div>
-                          <Image
-                            src={`https://avatars.dicebear.com/api/initials/${entry.user}.svg`}
-                            alt={entry.user}
-                            width={30}
-                            height={30}
-                            className="rounded-full"
-                          />
-                        </div>
-                        <div>{entry.user}</div>
-                      </div>
-                    </td>
-                    <td className="py-8 px-10 border-r border-gray-900 text-center">
-                      {entry.casts}
-                    </td>
-                    <td className="py-8 px-10 border-r border-gray-900  text-center">
-                      {entry.recasts}
-                    </td>
-                    <td className="py-8 px-10 border-r border-gray-900  text-center">
-                      {entry.likes}
-                    </td>
-                    <td className="py-8 px-10 border-r border-gray-900  text-center">
-                      {entry.replies}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+        </div>
+        <div
+          className={`grid grid-cols-1 md:grid-cols-12 gap-0 bg-green-600  text-center border border-black ${kreadonDemi.className}`}
+        >
+          <div className="col-span-1 md:col-span-1 md:border-r py-2 md:py-6 border-black px-2 lg:px-6 ">
+            Rank
           </div>
-          <div
-            className={`flex justify-between items-center mt-10 ${kreadonBold.className}`}
-          >
-            <button className="bg-green-600 text-black py-2 px-4 uppercase hover:bg-green-700 font-bold text-sm max-h-[35px]">
-              Create Frame
-            </button>
+          <div className="col-span-1 md:col-span-3 md:border-r py-2 md:py-6 border-black px-2 lg:px-6 ">
+            User
+          </div>
+
+          <div className="col-span-1 md:col-span-1 md:border-r py-2 md:py-6 border-black px-2 lg:px-6 ">
+            Casts
+          </div>
+          <div className="col-span-1 md:col-span-1 md:border-r py-2 md:py-6 border-black px-2 lg:px-6 ">
+            Recasts
+          </div>
+          <div className="col-span-1 md:col-span-1 md:border-r py-2 md:py-6 border-black px-2 lg:px-6 ">
+            Likes
+          </div>
+          <div className="col-span-1 md:col-span-1 md:border-r py-2 md:py-6 border-black px-2 lg:px-6 ">
+            Replies
+          </div>
+          <div className="col-span-1 md:col-span-2 md:border-r py-2 md:py-6 border-black px-2 lg:px-6 ">
+            Total Engagement
+          </div>
+          <div className="col-span-1 md:col-span-2 py-2 md:py-6">
+            Engagement Score
+          </div>
+        </div>
+        <div
+          className={`max-h-[calc(100vh-560px)] md:max-h-[calc(100vh-400px)] bg-black overflow-y-auto ${kreadonDemi.className} border-[0.01px] border-t-0 border-[#FEFAE0] border-opacity-50`}
+        >
+          {leaderboardData.map((item, index) => (
             <div
-              className={`flex items-center space-x-10 ${kreadonBold.className} text-[#FEFAE0]`}
+              key={index}
+              className="grid grid-cols-1 md:grid-cols-12 gap-y-0 text-center items-center  border-r border-[#FEFAE0] border-opacity-50 border-b md:border-b-0"
             >
-              <div
-                className="flex items-center"
-                onClick={() => paginate("prev")}
-              >
-                <FaCaretLeft size={24} /> PREVIOUS
+              <div className="col-span-1 md:col-span-1 border-r border-[#FEFAE0] border-opacity-50 py-6">
+                {item.rank}
+              </div>
+              <div className="col-span-1 md:col-span-3 border-r border-[#FEFAE0] border-opacity-50 py-6">
+                <div className="flex items-center space-x-2 justify-center md:justify-start">
+                  <div className="md:ml-4">
+                    <Image
+                      src={`https://avatars.dicebear.com/api/initials/${item.user}.svg`}
+                      alt={item.user}
+                      width={24}
+                      height={24}
+                      className="rounded-full"
+                    />
+                  </div>
+
+                  <div>{item.user}</div>
+                </div>
               </div>
 
-              <div
-                className="flex items-center "
-                onClick={() => paginate("next")}
-              >
-                NEXT <FaCaretRight size={24} />
+              <div className="col-span-1 md:col-span-1 border-r border-[#FEFAE0] border-opacity-50 py-6">
+                {item.casts}
               </div>
+              <div className="col-span-1 border-r border-[#FEFAE0] border-opacity-50 py-6">
+                {item.recasts}
+              </div>
+              <div className="col-span-1 border-r border-[#FEFAE0] border-opacity-50 py-6">
+                {item.likes}
+              </div>
+              <div className="col-span-1 border-r border-[#FEFAE0] border-opacity-50 py-6">
+                {item.replies}
+              </div>
+              <div className="col-span-1 md:col-span-2 border-r border-[#FEFAE0] border-opacity-50 py-6">
+                {123}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div
+          className={`flex justify-between items-center mt-10 ${kreadonBold.className}`}
+        >
+          <button className="bg-green-600 text-black py-2 px-4 uppercase hover:bg-green-700 font-bold text-sm max-h-[35px]">
+            Create Frame
+          </button>
+          <div
+            className={`flex items-center space-x-10 ${kreadonBold.className} text-[#FEFAE0]`}
+          >
+            <div className="flex items-center" onClick={() => paginate("prev")}>
+              <FaCaretLeft size={24} /> PREVIOUS
+            </div>
+
+            <div
+              className="flex items-center "
+              onClick={() => paginate("next")}
+            >
+              NEXT <FaCaretRight size={24} />
             </div>
           </div>
         </div>
