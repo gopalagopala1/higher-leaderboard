c5043435a4f69d7a9af45a4cfe26aa2fcc7ee763
gopalaxdev <agrawalgopala@gmail.com>
2024-06-24 15:20:17 +0530

    fix: type import

--- src/app/api/fetchUsers/route.ts
+++ src/app/api/fetchUsers/route.ts
@@ -1,37 +1,39 @@
-import { UserData } from "@/components/leaderboard";
+import { UserData } from "@/app/types/types";
 import { NextRequest, NextResponse } from "next/server";
 
 interface RequestBody {
-    fids: number[];
+  fids: number[];
 }
 
 export interface UserRes {
-    users: UserData[]
+  users: UserData[];
 }
 export async function POST(req: NextRequest, res: NextResponse) {
-    try {
+  try {
+    const reqBody: RequestBody = await req.json();
+    const fids = reqBody.fids;
 
-        const reqBody: RequestBody = await req.json();        
-        const fids = reqBody.fids
+    const fidsString = fids.join("%2C%20");
 
-        const fidsString = fids.join('%2C%20');
+    const res = await fetch(
+      `https://api.neynar.com/v2/farcaster/user/bulk?fids=${fidsString}`,
+      {
+        headers: {
+          api_key: process.env.NEYNAR_API_KEY!,
+        },
+      }
+    );
 
-        const res = await fetch(`https://api.neynar.com/v2/farcaster/user/bulk?fids=${fidsString}`, {
-            headers: {
-                'api_key': process.env.NEYNAR_API_KEY!
-            }
-        })
+    const data: UserRes = await res.json();
 
-        const data: UserRes = await res.json();
-
-        const jsonData = JSON.stringify(data.users);
-        return new NextResponse(jsonData, {
-            status: 200,
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-    } catch (err: any) {
-        return new NextResponse('Failed to fetch leaderboard', { status: 500 })
-    }
+    const jsonData = JSON.stringify(data.users);
+    return new NextResponse(jsonData, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err: any) {
+    return new NextResponse("Failed to fetch leaderboard", { status: 500 });
+  }
 }
