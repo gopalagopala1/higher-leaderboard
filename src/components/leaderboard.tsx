0d183d1aa810ac3903d74a5fef41fd279842fdbb
tusharverma06 <vermatushar237@gmail.com>
2024-06-29 17:25:58 +0530

    formatted stats for logged in user too

--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -402,7 +402,7 @@ export default function Leaderboard() {
                       <div className="col-span-1 md:col-span-3  border-[#FEFAE0] border-opacity-50 py-4">
                         {item?.Engagement_Score >= 1000
                           ? `${(Number(item?.Engagement_Score) / 1000).toFixed(2)}k`
-                          : item?.Engagement_Score}
+                          : item?.Engagement_Score.toFixed(2)}
                       </div>
                     </div>
                   );
@@ -458,19 +458,29 @@ export default function Leaderboard() {
                     </div>
                   </div>
                   <div className="col-span-1 md:col-span-1  border-[#FEFAE0] border-opacity-50 py-4">
-                    {loggedInUserRank?.count_recasts}
+                    {loggedInUserRank.count_recasts >= 1000
+                      ? `${(Number(loggedInUserRank.count_recasts) / 1000).toFixed(2)}k`
+                      : loggedInUserRank.count_recasts}
                   </div>
                   <div className="col-span-1 md:col-span-1  border-[#FEFAE0] border-opacity-50 py-4">
-                    {loggedInUserRank?.count_likes}
+                    {loggedInUserRank.count_likes >= 1000
+                      ? `${(Number(loggedInUserRank.count_likes) / 1000).toFixed(2)}k`
+                      : loggedInUserRank.count_likes}
                   </div>
                   <div className="col-span-1 md:col-span-1  border-[#FEFAE0] border-opacity-50 py-4">
-                    {loggedInUserRank?.count_replies}
+                    {loggedInUserRank.count_replies >= 1000
+                      ? `${(Number(loggedInUserRank.count_replies) / 1000).toFixed(2)}k`
+                      : loggedInUserRank.count_replies}
                   </div>
                   <div className="col-span-1 md:col-span-2  border-[#FEFAE0] border-opacity-50 py-4">
-                    {loggedInUserRank?.total_engagement}
+                    {loggedInUserRank.total_engagement >= 1000
+                      ? `${(Number(loggedInUserRank.total_engagement) / 1000).toFixed(2)}k`
+                      : loggedInUserRank.total_engagement}
                   </div>
                   <div className="col-span-1 md:col-span-3  border-[#FEFAE0] border-opacity-50 py-4">
-                    {loggedInUserRank?.Engagement_Score.toFixed(2)}
+                    {loggedInUserRank.Engagement_Score >= 1000
+                      ? `${(Number(loggedInUserRank.Engagement_Score) / 1000).toFixed(2)}k`
+                      : loggedInUserRank.Engagement_Score.toFixed(2)}
                   </div>
                 </div>
               )}
